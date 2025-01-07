37b5841874d5e9bca46119555f82e5d11cd93551
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-01-07 18:24:44 +0500

    Added book edit view and updated another views

--- apps/views/books.py
+++ apps/views/books.py
@@ -1,22 +1,25 @@
+from urllib import request
+
+from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
 from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.urls.base import reverse
-from django.views.generic import ListView, CreateView, DeleteView, FormView, DetailView
-
+from django.views.generic import ListView, CreateView, DeleteView, FormView, DetailView, UpdateView
+from accounts.models import User
 from accounts.mixins import LoginAndVerificationRequiredMixin
 from apps.forms import BookForm, ReviewCreateForm
 from apps.models import Book, Category, Review
 
 
 class AllBooksListView(ListView):
+    model = Book
+    context_object_name = 'books'
     template_name = 'booksaw/all_books.html'
-    queryset = Book.objects.all()
-    paginator_class = Paginator(object_list=queryset, per_page=100)
-
+    categories = Category.objects.all()
     extra_context = {
-        'categories': Category.objects.all(),
+        'categories': categories,
     }
 
 
@@ -36,10 +39,21 @@ class BookCreateView(LoginAndVerificationRequiredMixin, CreateView):
 
 
 class MyBooksListView(LoginAndVerificationRequiredMixin, ListView):
+    model = Book
+    context_object_name = 'books'
     template_name = 'booksaw/my_books.html'
 
-    def get_queryset(self):
+    def get_queryset(self, **kwargs):
+        """Foydalanuvchiga tegishli kitoblarni olish."""
         return self.request.user.books.all()
+
+    def get_context_data(self, **kwargs):
+        """Qo'shimcha kontekst ma'lumotlarni qo'shish."""
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['profile'] = self.request.user  # Profilni olish
+        return context
+
 
 
 class BookDeleteView(LoginAndVerificationRequiredMixin, DeleteView):
@@ -74,3 +88,18 @@ class BookDetailView(LoginAndVerificationRequiredMixin,FormView, DetailView):
 
     def get_success_url(self):
         return reverse('book_detail', kwargs={'pk': self.object.pk})
+
+class BookUpdateView(LoginAndVerificationRequiredMixin, UpdateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'booksaw/book_update.html'
+    success_url = reverse_lazy('my_books')
+    def post(self, request, *args, **kwargs):
+        book = self.get_object()
+        if book.owner != request.user:
+            return PermissionDenied("You do not have permission to delete this book.")
+        form = self.form_class(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            form.save()
+            return  HttpResponseRedirect(self.success_url)
+        return render(request, self.template_name, {'form': form})
