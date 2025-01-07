42dfd15a6ca3325e2c38a039fa9ee097b1fa4b84
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-01-07 18:28:28 +0500

    Updated home view

--- apps/views/home.py
+++ apps/views/home.py
@@ -1,10 +1,6 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.paginator import Paginator
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import ListView
 
-from apps.forms import BookForm
-from apps.models import Book, Category
+from apps.models import Book, Category, Cart
 
 """
 GENERIC VIEWS
@@ -22,5 +18,21 @@ DeleteView
 """
 
 
-class HomeTemplateView(TemplateView):
+class HomeTemplateView(ListView):
+    model = Book
+    categories = Category.objects.all()
+    queryset = Book.objects.filter(availability='On sale').order_by('-id')[:8]
+    context_object_name = 'books'
+    last_books = Book.objects.filter(availability='On sale').order_by('-average_rating')[:10]
+    featured_books = Book.objects.filter(average_rating__gte=4, average_rating__lte=5,
+                                         availability='On sale').order_by('-average_rating')[:4]
+    extra_context = {
+        'last_books': last_books,
+        'featured_books': featured_books,
+        'categories': categories,
+    }
     template_name = 'booksaw/index.html'
+
+    def post(self, request, *args, **kwargs):
+        cart = self.request.session.get('cart', None)
+
