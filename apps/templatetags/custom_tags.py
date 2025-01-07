5ec332535fac247a00fe305b774740b054da7f82
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-01-07 18:20:04 +0500

    Added  templatetags

--- apps/templatetags/custom_tags.py
+++ apps/templatetags/custom_tags.py
@@ -1,20 +1,26 @@
 # your_app/templatetags/custom_filters.py
 from django import template
-from books_app.models import Books
+from apps.models import Book
 
 register = template.Library()
 
 @register.simple_tag
 def get_item(category_name):
 
-    return Books.objects.filter(
+    return Book.objects.filter(
         availability='On sale',
         category__name=category_name # ManyToMany orqali bog'langan 'name' maydonini tekshirish
     ).order_by('-id')[:8]
 
 @register.simple_tag
 def get_item_book(category_name):
-    return Books.objects.filter(
+    return Book.objects.filter(
         availability='On sale',
         category__name=category_name  # ManyToMany orqali bog'langan 'name' maydonini tekshirish
     ).order_by('-id')[:100]
+
+@register.simple_tag
+def get_my_book(category_name):
+    return Book.objects.filter(
+        category__name=category_name  # ManyToMany orqali bog'langan 'name' maydonini tekshirish
+    ).order_by('-id')
