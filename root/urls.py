9bcd05cc780a84aa0a6e8dad8e46ca0ec716a151
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-01-07 18:37:38 +0500

    Added django auth urls

--- root/urls.py
+++ root/urls.py
@@ -25,8 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include(('apps.urls', 'apps'), namespace='apps')),
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', HomeTemplateView.as_view(), name='home'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
